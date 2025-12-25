00bedb27ddbe73fc874a20fff8e904bcabcbdebc
Alexander Kvamme <alexanderkvamme@gmail.com>
2025-12-25 17:31:30 +0100

    Only ios

--- Package.swift
+++ Package.swift
@@ -4,10 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Featurefest",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v13)
     ],
     products: [
         .library(
